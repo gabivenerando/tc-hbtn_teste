8265cbfe1ce8923a5c9a8a4477b75647cd5c289f
Gabrielli Venerando <107216833+gabivenerando@users.noreply.github.com>
2022-07-20 16:34:18 -0300

    Add files via upload

---
+++ 0x02/password/Person.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+
+public class Person {
+
+    public boolean checkUser(String username){
+        return username.length() > 8 && !username.matches("^(?=.*[@!#$%^&*()/\\\\])");
+    }
+
+    public boolean checkPassword(String password){
+        return password.matches("^(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*., ?]).+$") && password.matches("^(?=.*[@!#$%^&*()/\\\\])") && password.length() >= 8;
+    }
+}
